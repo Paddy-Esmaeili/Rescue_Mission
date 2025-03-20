ecc926087516f7995576f227f0bca772543800f8
rosamariaaaa <collir23@mcmaster.ca>
2025-03-20 15:10:42 -0400

    comments and log messages

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/GridSearch.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/GridSearch.java
@@ -19,21 +19,37 @@ import ca.mcmaster.se2aa4.island.teamXXX.Direction;
 public class GridSearch implements Searcher {
 
     /**
-     * Action Types
+     * Mode
+     * An enumeration of all potential action types.
+     * FLY - Move forward one space
+     * SCAN - Scan surroundings
+     * CONTINUE - After scanning, move forward one space.
+     * RIGHT TURN - Turn right relative to the current direction
+     * LEFT TURN - Turn left relative to the current direction
      */
     private enum Mode {
-        FLY("action", "fly"),
-        SCAN("action", "scan"),
-        CONTINUE("action", "fly"),
-        RIGHT_TURN("action", "heading"),
-        LEFT_TURN("action", "heading");
+        FLY("action", "fly", "flying forward"),
+        SCAN("action", "scan", "scanning surroundings"),
+        CONTINUE("action", "fly", "continuing"),
+        RIGHT_TURN("action", "heading", "turning right"),
+        LEFT_TURN("action", "heading", "turning left");
 
-        String type;
-        String item;
+        private String type;
+        private String item;
+        private String str;
+        private static final Logger logger = LogManager.getLogger();
 
-        Mode(String type, String item) {
+        Mode(String type, String item, String str) {
             this.type = type;
             this.item = item;
+            this.str = str;
+        }
+
+        /**
+         * Get the mode in string form for log messages
+         */
+        public String toString() {
+            return str;
         }
 
         /**
@@ -63,44 +79,62 @@ public class GridSearch implements Searcher {
 
     }
 
+    private static final Logger logger = LogManager.getLogger();
     private DirectionStrategy direction;
     private Mode mode;
     private boolean creekFound;
     private boolean siteFound;
 
     public GridSearch(FindIsland island) {
+        logger.info("Instantiating Grid Search. MODE: FLY");
         mode = Mode.FLY;
         creekFound = false;
         siteFound = false;
     }
 
+    /**
+     * Return true if the grid search is complete. 
+     */
     @Override
     public boolean isComplete() {
         return creekFound && siteFound; 
     }
-    
+
+    /**
+     * Return a JSON object containing next move data.
+     */
     public JSONObject getDecision() {
         return mode.getDecision(direction);
     }
 
+    /**
+     * Switch decision modes based on the results of the previous action.
+     */
     public Mode switchMode() {
         if (mode == Mode.FLY) {
+            logger.info("Switching to SCAN mode");
             return Mode.SCAN;
         }
         else if (mode == Mode.SCAN) {
+            logger.info("Scan complete. Proceeding in CONTINUE mode");
             return Mode.CONTINUE;
         }
         else if (mode == Mode.CONTINUE) {
+            logger.info("Switching to FLY mode");
             return Mode.FLY;
         }
         else 
             return null;
     }
 
+    /**
+     * After a decision is made, process results.
+     */
     public void processResponse(String responseString) {
         JSONObject response = new JSONObject(responseString);
 
         if (mode == Mode.SCAN) {
+            logger.info("Evaluating SCAN data");
             checkScan(responseString);
         }
 
